1d5eabdf1a4f0d6d64b4798f17d85c66fde74881
UnderKo <hhit934@gmail.com>
2022-02-10 13:50:16 +0300

    A

---
+++ Hardware/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hardware
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += (o, e) =>
+            {
+                new WinDragMove(this, this);
+                new WinResize(this).RightDown(resizeborder);
+                new WBControl(this, bclose, ACTIONS.CLOSE);
+                new WBControl(this, bmax, ACTIONS.SIZE_max);
+                new WBControl(this, bmin, ACTIONS.SIZE_min);
+                new WindowBlureffect(this, WindowBlureffect.AccentState.ACCENT_ENABLE_BLURBEHIND);
+
+            };
+        }
+    }
+}
